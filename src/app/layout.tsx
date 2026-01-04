8048c96f59232f95ed43039cb7dfc9287856622d
Derek Osgood <osgood.derek@gmail.com>
2026-01-03 18:28:05 -0800

    fix: add TooltipProvider to root layout
    Tooltips require TooltipProvider from Radix UI to be present in the component tree

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Crimson_Pro, Inter, JetBrains_Mono } from 'next/font/google';
 import { Toaster } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryProvider } from '@/components/providers/query-provider';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import './globals.css';
@@ -40,8 +41,10 @@ export default function RootLayout({
       >
         <ThemeProvider>
           <QueryProvider>
-            {children}
-            <Toaster />
+            <TooltipProvider>
+              {children}
+              <Toaster />
+            </TooltipProvider>
           </QueryProvider>
         </ThemeProvider>
       </body>
